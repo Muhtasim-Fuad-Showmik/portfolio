1c60ee78a939707a2e972914f577171135ab2edd
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2022-07-03 15:05:47 +0600

    Updated image URLs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'BodyWrap',
     description: "An ecommerce website that is tailored towards selling apparel items online. The project is made using Mongoose, Express, React and Node JS. It has security implementations using AES hashing and also has an admin dashboard prepared for the business owner.",
-      image: '/images/bodywrap.png',
+      image: 'https://i.ibb.co/GPn2NC2/bodywrap.png',
       tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://github.com/Muhtasim-Fuad-Showmik/BodyWrap',
     visit: 'https://google.com',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Tent',
     description:"A website that allows users to search for new campgrounds to visit, review campgrounds existing on the system, as well as add new campgrounds to the system. The system is built using Mongoose, Express and Node JS and has several security implementations and map implementations on it. For a sample credential to use for the site you may use the username of \"Guest\" and the password \"guestpass\".",
-    image: '/images/tent.png',
+    image: 'https://i.ibb.co/xj0p7XS/tent.png',
     tags: ['Mongo', 'Express', 'Node'],
     source: 'https://github.com/Muhtasim-Fuad-Showmik/Tent',
     visit: 'http://powerful-river-65642.herokuapp.com/',
