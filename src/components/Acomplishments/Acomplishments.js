d4ba88db8fb1df571cfe747fecc46ada1f0ac81f
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-11-07 00:54:35 +0600

    Updated "Accomplishments"

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 2.5, more: '', text: 'Years of Professional Experience'},
-  { number: 5, more: '+', text: 'Contests', },
-  { number: 20, more: '+', text: 'Projects', }
+  { number: 3, more: "", text: "Years of Professional Experience" },
+  { number: 10, more: "+", text: "Contests" },
+  { number: 30, more: "+", text: "Projects" },
 ];
 
 const Acomplishments = () => (
@@ -15,7 +19,10 @@ const Acomplishments = () => (
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
-          <BoxNum>{card.number}{card.more}</BoxNum>
+          <BoxNum>
+            {card.number}
+            {card.more}
+          </BoxNum>
           <BoxText>{card.text}</BoxText>
         </Box>
       ))}
